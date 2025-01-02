e90d822308e750c6e180b92d80f28641f339e761
Balázs Varga <balazs.varga@apter.tech>
2025-01-03 00:39:10 +0100

    refactor(deploy, post_action): enhance container management and streamline post-cleanup logic 	•	deploy.sh: 	•	Parameterized container naming using ${RUNNER_OS} for clarity in multi-platform scenarios. 	•	Added unset for WHOAMI_VERSION and CONTAINER_NAME to ensure no residual environment variables. 	•	post_action.sh: 	•	Commented out unnecessary rm and related notices, retaining placeholder for future cleanup logic.
    These changes improve maintainability, adaptability for diverse environments, and ensure clean execution contexts.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 # renovate: datasource=docker depname=traefik/whoami versioning=docker
 WHOAMI_VERSION="v1.10"
-docker run --detach --publish-all --name whoami traefik/whoami:"${WHOAMI_VERSION}"
-docker stop whoami
-docker rm whoami
+CONTAINER_NAME="${RUNNER_OS}-whoami"
+docker run --detach --publish-all --name "${CONTAINER_NAME}" traefik/whoami:"${WHOAMI_VERSION}"
+docker stop "${CONTAINER_NAME}"
+docker rm "${CONTAINER_NAME}"
+
+unset WHOAMI_VERSION
+unset CONTAINER_NAME
