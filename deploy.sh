c80cd9403f84d7cace72820ad926ab23892b2da8
Balázs Varga <balazs.varga@apter.tech>
2025-01-02 23:58:14 +0100

    refactor(workflow): standardize script execution and modularize deployment 	•	Replaced direct script executions with sh for consistency across different environments. 	•	Introduced deploy.sh to encapsulate deployment logic, improving maintainability and reusability. 	•	Removed inline deployment commands from the workflow to streamline and declutter the YAML file. 	•	Enhanced readability and consistency across all script invocations in the workflow.

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# renovate: datasource=docker depname=traefik/whoami versioning=docker
+WHOAMI_VERSION="43a68d10b9dfcfc3ffbfe4dd42100dc9aeaf29b3a5636c856337a5940f1b4f1c" # v1.10
+docker run --detach --publish-all --name whoami traefik/whoami:"${WHOAMI_VERSION}"
+docker stop whoami
+docker rm whoami
