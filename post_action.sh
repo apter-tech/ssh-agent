3deb2d09d80c7453e46bbb2b7acdb22d6198afb0
Balázs Varga <balazs.varga@apter.tech>
2025-01-03 00:39:10 +0100

    refactor(deploy, post_action): enhance container management and streamline post-cleanup logic 	•	deploy.sh: 	•	Parameterized container naming using ${RUNNER_OS} for clarity in multi-platform scenarios. 	•	Added unset for WHOAMI_VERSION and CONTAINER_NAME to ensure no residual environment variables. 	•	post_action.sh: 	•	Commented out unnecessary rm and related notices, retaining placeholder for future cleanup logic.
    These changes improve maintainability, adaptability for diverse environments, and ensure clean execution contexts.

--- post_action.sh
+++ post_action.sh
@@ -3,7 +3,7 @@
 if [ -z "${SSH_KNOWN_HOSTS_FILE}" ]; then
     echo "::error file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Notice::\
 ${SSH_KNOWN_HOSTS_FILE} environment variable must be set."
-else
+#else
     # rm -rf "${SSH_KNOWN_HOSTS_FILE}"
     #echo "::notice file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Notice::\
 #${SSH_KNOWN_HOSTS_FILE} has been removed."
