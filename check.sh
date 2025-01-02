5ff88eedec568391822cd2ce4b7b3cded36936a5
Balázs Varga <balazs.varga@apter.tech>
2025-01-02 21:55:28 +0100

    fix: Enhance error handling and notices in SSH setup scripts
    - Added `set -e` to `action.sh` for immediate exit on error.
    - Improved log notices for created known hosts files.
    - Adjusted conditional checks in `check.sh` and `post_check.sh` to use `[[` for better compatibility.

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -s ~/.ssh/known_hosts ]; then
+if [[ ! -s ~/.ssh/known_hosts ]]; then
     echo "::error file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Assertion Error::\
 ~/.ssh/known_hosts is missing or empty."
     exit 1
