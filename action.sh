64e21dfb8170744b79e5b9488883945e7fd6dd95
Balázs Varga <balazs.varga@apter.tech>
2025-01-02 18:43:58 +0100

    feat: Add custom SSH agent action for workflows (#1)
    - Introduced `action.sh` to manage SSH host key validation and `known_hosts` setup.
    - Added `action.yml` for defining a composite GitHub Action, supporting both predefined and dynamically scanned SSH hosts.
    - Implemented `post_action.sh` to clean up `known_hosts` file post-deployment for enhanced security.
    - Enhanced validation with warnings and errors for invalid or missing inputs.
    - Improved security with support for SSH key type filtering and explicit warnings for potential MITM risks.
    - Integrated `webfactory/ssh-agent` for managing private SSH keys in workflows.

---
+++ action.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [ -z "${SSH_HOST}" ] && [ -z "${SSH_KNOWN_HOSTS}" ]; then
+    echo ":error file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Input validation::\
+One of the input, 'ssh-host' or 'ssh-known-hosts' must be set. Please update your workflow inputs."
+    exit 2
+else
+    if [ ! -z "${SSH_KNOWN_HOSTS}" ]; then
+        if [ ! -z "${SSH_HOST}" ]; then
+            echo "::warning file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Input validation::\
+Both 'ssh-host' and 'ssh-known-hosts' inputs are set. Using 'ssh-known-hosts'."
+        fi
+        echo "${SSH_KNOWN_HOSTS}" >> "${SSH_KNOWN_HOSTS_FILE}"
+    else
+        echo "::warning file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Security risk::\
+If an ssh_known_hosts file is constructed using ssh-keyscan without verifying the keys, \
+users will be vulnerable to man in the middle attacks. On theother hand, if the security model allows such a risk, \
+ssh-keyscan can help in the detection of tampered keyfiles or man in the middle attacks which have begun after \
+the ssh_known_hosts file was created."
+    
+        if [ -z "${SSH_KEY_TYPE}" ]; then
+            if ! ssh-keyscan "${SSH_HOST}" >> "${SSH_KNOWN_HOSTS_FILE}"; then
+                echo "::error file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=SSH Keyscan Failed::\
+Failed to scan SSH host keys for ${SSH_HOST}"
+                exit 1
+            fi
+        else
+            if ! ssh-keyscan -t "${SSH_KEY_TYPE}" "${SSH_HOST}" >> "${SSH_KNOWN_HOSTS_FILE}"; then
+                echo "::error file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=SSH Keyscan Failed::\
+Failed to scan SSH host keys for ${SSH_HOST}"
+                exit 1
+            fi
+        fi
+    fi
+fi
