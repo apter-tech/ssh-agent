1c9dbd0b4ca26fbb0543894b10f821f16ba0525a
Balázs Varga <balazs.varga@apter.tech>
2025-01-03 00:40:33 +0100

    x

--- post_check.sh
+++ post_check.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -s "${HOME}/.ssh/known_hosts" ]; then
+    echo "ok"
     #echo "::error file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Assertion Error::\
 #~/.ssh/known_hosts file should not exist after the job."
     #exit 1
