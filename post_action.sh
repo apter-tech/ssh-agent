07fdbb0069796a197d9ce9fd0b3552c532c87fd3
Balázs Varga <balazs.varga@apter.tech>
2025-01-03 11:06:04 +0100

    fix(post_action): ensure consistent variable definition for known hosts 	•	Defined SSH_KNOWN_HOSTS_FILE explicitly in post_action.sh for clarity and consistency. 	•	Maintained existing temporary file logic for truncating the last line from the known_hosts file. 	•	Improved readability by ensuring all required variables are explicitly initialized within the script.

--- post_action.sh
+++ post_action.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+SSH_KNOWN_HOSTS_FILE="${HOME}/.ssh/known_hosts"
 TEMP_FILE="/tmp/718f4157-5493-43b2-837b-3ccb27f78e7b"
 
 head --lines=-1 "${SSH_KNOWN_HOSTS_FILE}" > "${TEMP_FILE}"
@@ -9,4 +10,5 @@ rm -rf "${TEMP_FILE}"
 echo "::notice file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Notice::\
 ${SSH_KNOWN_HOSTS_FILE} has been cleaned."
 
+unset SSH_KNOWN_HOSTS_FILE
 unset TEMP_FILE
