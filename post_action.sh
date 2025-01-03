286a349beed232b229c6ad1a7a4ec4f96ef62a17
Balázs Varga <balazs.varga@apter.tech>
2025-01-03 15:38:32 +0100

    refactor(workflow): enhance SSH setup and cleanup logic (#11)
    - Reorganized the workflow to improve clarity and maintainability:
      - Added a new `noop.sh` script for setup post-check of known hosts.
      - Moved the `Setup SSH key` step after `Check known hosts file` for better sequence.
    - Improved the `check.sh` script:
      - Validates the presence of expected SSH fingerprints in the `known_hosts` file.
    - Updated the `post_action.sh` script:
      - Simplified removal of the `known_hosts` file instead of line-by-line cleanup.
    - Adjusted `post_check.sh` to ensure the `known_hosts` file is completely removed.
    - Updated `action.yml` to include corrected paths and a reordering of the steps.

--- post_action.sh
+++ post_action.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env sh
 
 SSH_KNOWN_HOSTS_FILE="${HOME}/.ssh/known_hosts"
-TEMP_FILE="/tmp/718f4157-5493-43b2-837b-3ccb27f78e7b"
 
-sed '$ d' "${SSH_KNOWN_HOSTS_FILE}" > "${TEMP_FILE}"
-cat "${TEMP_FILE}" > "${SSH_KNOWN_HOSTS_FILE}"
-rm -rf "${TEMP_FILE}"
+rm -rf "${SSH_KNOWN_HOSTS_FILE}"
 
 echo "::notice file=$(basename "$0"),line=${LINENO},endLine=${LINENO},title=Notice::\
-${SSH_KNOWN_HOSTS_FILE} has been cleaned."
+${SSH_KNOWN_HOSTS_FILE} has been removed."
 
 unset SSH_KNOWN_HOSTS_FILE
-unset TEMP_FILE
