7406251ff05ebc44026b9e04c695f19316d93fde
Balázs Varga <balazs.varga@apter.tech>
2025-01-03 11:13:22 +0100

    fix(post_action): correct sed syntax for removing the last line 	•	Removed unnecessary file argument in the sed command. 	•	Ensured the correct syntax is used for processing the SSH_KNOWN_HOSTS_FILE. 	•	Improved reliability and accuracy of the script by properly handling file processing.

--- post_action.sh
+++ post_action.sh
@@ -3,7 +3,7 @@
 SSH_KNOWN_HOSTS_FILE="${HOME}/.ssh/known_hosts"
 TEMP_FILE="/tmp/718f4157-5493-43b2-837b-3ccb27f78e7b"
 
-sed '$ d' file "${SSH_KNOWN_HOSTS_FILE}" > "${TEMP_FILE}"
+sed '$ d' "${SSH_KNOWN_HOSTS_FILE}" > "${TEMP_FILE}"
 cat "${TEMP_FILE}" > "${SSH_KNOWN_HOSTS_FILE}"
 rm -rf "${TEMP_FILE}"
 
